3b33648050e4bc1922be287fa839adc1cc73c689
Nick Cast <mentossstack@gmail.com>
2024-12-22 22:34:32 +0200

    Added Tables, DB and Forms.

--- forms.py
+++ forms.py
@@ -1,6 +1,24 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
+from wtforms import StringField, SubmitField, PasswordField, IntegerField
 from wtforms.validators import DataRequired, URL, Email
 from flask_ckeditor import CKEditorField
 
+class SignUpForm(FlaskForm):
+    name = StringField(label="Name", validators=[DataRequired()])
+    email = StringField(label="Email", validators=[DataRequired(), Email()])
+    password = PasswordField(label="Password", validators=[DataRequired()])
+    submit = SubmitField(label='Sign Up')
 
+class LoginForm(FlaskForm):
+    email = StringField(label="Email", validators=[DataRequired(), Email()])
+    password = PasswordField(label="Password", validators=[DataRequired()])
+    submit = SubmitField(label='Log In')
+
+class PostCommentForm(FlaskForm):
+    text = CKEditorField(label="Your Comment", validators=[DataRequired()])
+    rating = IntegerField("Your Rating", validators=[DataRequired()])
+    submit = SubmitField(label='Post')
+
+class FindMovieForm(FlaskForm):
+    name = StringField(label="Movie Name", validators=[DataRequired()])
+    submit = SubmitField(label='Search')
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, redirect, url_for, request
-from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import Integer, String, Float
-from wtforms.validators import DataRequired
+from flask_login import login_required, login_user, logout_user, UserMixin, current_user, LoginManager
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_ckeditor import CKEditor
 from os import getenv
 from datetime import date
@@ -15,7 +15,6 @@ import requests
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///movies.db"
 app.config['SECRET_KEY'] = getenv("SECRET_KEY")
-Bootstrap5(app)
 
 API_KEY = getenv('API_KEY')
 API_SEARCH_URL = 'https://api.themoviedb.org/3/search/movie'
@@ -28,6 +27,48 @@ db = SQLAlchemy(app)
 
 # CKEditor
 ckeditor = CKEditor(app)
+
+# DB | TABLE DATA
+
+from sqlalchemy import func
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    email: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String, nullable=False)
+    comments: Mapped[str] = relationship("Comment", back_populates="comment_author")
+    movies_added: Mapped[str] = relationship("Movie", back_populates="contributor")
+
+class Movie(db.Model):
+    __tablename__ = 'movies'
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    year: Mapped[int] = mapped_column(Integer, nullable=False)
+    description: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    comments: Mapped[str] = relationship('Comment', back_populates='parent_movie')
+    
+    @property
+    def average_rating(self):
+        ratings = [comment.rating for comment in self.comments]
+        if ratings:
+            return sum(ratings) / len(ratings)
+        return 0  
+
+class Comment(db.Model):
+    __tablename__ = "comments"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    author_id: Mapped[int] = mapped_column(Integer, db.ForeignKey('users.id'), nullable=False)
+    comment_author: Mapped[str] = relationship('User', back_populates='comments')
+    movie_id: Mapped[int] = mapped_column(Integer, db.ForeignKey('movies.id'), nullable=False)
+    parent_movie: Mapped[str] = relationship('Movie', back_populates='comments')
+    text: Mapped[str] = mapped_column(String, nullable=False)
+    rating: Mapped[int] = mapped_column(Integer, nullable=True)  # Rating added by user
+
+with app.app_context():
+    db.create_all()
 
 # ROUTES
 
