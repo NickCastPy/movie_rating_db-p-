5ea559bf8818bd29c93a413af93ca6573b035fe7
Nick Cast <mentossstack@gmail.com>
2024-12-22 20:44:40 +0200

    Initialised the imports & crucial variables

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
+from wtforms.validators import DataRequired
+from flask_ckeditor import CKEditor
+from os import getenv
+from datetime import date
+import requests
+
+
+# APP & KEYS
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///movies.db"
+app.config['SECRET_KEY'] = getenv("SECRET_KEY")
+Bootstrap5(app)
+
+API_KEY = getenv('API_KEY')
+API_SEARCH_URL = 'https://api.themoviedb.org/3/search/movie'
+MOVIE_DB_INFO_URL = "https://api.themoviedb.org/3/movie"
+MOVIE_DB_IMAGE_URL = "https://image.tmdb.org/t/p/w500"
+
+# Set the DB
+
+db = SQLAlchemy(app)
+
+# CKEditor
+ckeditor = CKEditor(app)
+
+# ROUTES
 
 @app.route("/")
 def render_main():
